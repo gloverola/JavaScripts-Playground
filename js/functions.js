10ebf8805c0c9e8babf6f50c2f64bff90b38e4eb
copdev <glovermartial@gmail.com>
2021-01-16 19:15:01 +0100

    refactor

--- js/functions.js
+++ js/functions.js
@@ -407,3 +407,92 @@
 // };
 
 // print( runner );
+
+
+// ======= Shorthand Object Properties ======== 
+
+// const reviews = [ 4.5, 5.0, 3.44, 2.8, 3.5, 4.0, 3.5 ];
+
+// const getStats = ( arr ) => {
+//     const max = Math.max( ...arr );
+//     const min = Math.min( ...arr );
+//     const sum = arr.reduce( ( sum, r ) => sum + r );
+//     const avg = sum / arr.length;
+
+//     return {
+//         max,
+//         min,
+//         sum,
+//         avg,
+//     }
+// }
+
+// const stats = getStats( reviews );
+
+// console.log(stats);
+
+// ======== Computed Properties ==========
+
+// const role = 'host';
+// const person = 'Jools Holland';
+// const role2 = 'Director';
+// const person2 = 'James Cameron';
+
+// const team = {
+//     [ role ]: person,
+//     [ role2 ]: person2,
+//     [1 + 6 + 9]: 'sisteen'
+    
+// }
+
+// console.log(team);
+
+// ======== Adding Methods to objectts ======== 
+
+// const math = {
+//     multiply: function ( x, y ) {
+//         return x * y;
+//     },
+//     divide: function ( x, y ) {
+//         return x / y;
+//     },
+//     square: function ( x ) {
+//         return x * x;
+//     }
+// };
+
+// console.log(math.multiply(2, 4));
+
+// ============ THIS ===============
+
+// function sayHi () {
+//     console.log( "hi" );
+//     console.log(this);
+// }
+
+// sayHi()
+
+const myDeck = {
+    deck: [],
+    suits: [ 'hearts', 'diamonds', 'spades', 'clubs' ],
+    values: '2,3,4,5,6,7,8,9,10,J,Q,K,A',
+    initializeDeck () {
+        const { suits, values, deck } = this;
+
+        for ( let value of values.split( ',' ) )
+        {
+            for ( let suit of suits )
+            {
+                deck.push( {
+                    value,
+                    suit
+                })
+            }
+        }
+    },
+    drawCard () {
+        return this.deck.pop();
+    }
+}
+
+console.log(myDeck);
